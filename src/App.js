9ee74adeafc5ce2e1ff116740ae4489f9c8959b9
koomekevo <koome.kevo@gurucrafts.net>
2022-01-19 08:42:51 +0300

    Implement changes from reviewer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import BookContainer from './components/BookContainer';
 import AddBook from './components/AddBook';
 import Navbar from './components/Navbar';
-import store from './redux/store';
 
 function App() {
-  const books = store.getState();
   return (
     <Router>
       <Navbar />
       <Switch>
         <Route exact path="/">
-          <BookContainer books={books} />
           <AddBook />
         </Route>
         <Route path="/categories">
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,7 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './books/books';
-
-const reducer = combineReducers({
-  booksReducer,
-  // additional reducers could be added here
-});
 
 const store = createStore(
-  reducer,
   applyMiddleware(logger),
 );
 
